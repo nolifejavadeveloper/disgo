b325bb77ad18d280990cd585573898575e5bdfea
what <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-05-15 10:37:44 -0400

    added event bus logics to internal

---
+++ internal/event/bus.go
@@ -0,0 +1,23 @@
+package event
+
+import "reflect"
+
+type eventHandler func(any) error
+
+type Bus struct {
+	handlers map[reflect.Type][]eventHandler
+}
+
+func Subscribe[T any](b *Bus, handler func(T) error) {
+	var t T
+	typ := reflect.TypeOf(t)
+	b.handlers[typ] = append(b.handlers[typ], func(e any) error {
+		return handler(e.(T))
+	})
+}
+
+func Fire(b *Bus, event any) {
+	for _, handler := range(b.handlers[reflect.TypeOf(event)]) {
+		handler(event)
+	}
+}
