66d49f0ae07fea27c61075244ea1559bdf806fd4
what <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-05-15 14:00:57 -0400

    added get type by event type function

--- internal/event/event.go
+++ internal/event/event.go
@@ -1,6 +1,9 @@
 package event
 
-import "reflect"
+import (
+	"errors"
+	"reflect"
+)
 
 type Event interface {
 	Type() string
@@ -10,4 +13,13 @@ var registry map[string]reflect.Type = make(map[string]reflect.Type)
 
 func register(e Event) {
 	registry[e.Type()] = reflect.TypeOf(e).Elem()
+}
+
+func FindType(t string) (Event, error) {
+	typ, ok := registry[t]
+	if !ok {
+		return nil, errors.New("event type not found in registry")
+	}
+
+	return reflect.New(typ).Interface().(Event), nil
 }
