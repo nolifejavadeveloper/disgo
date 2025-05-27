4ecf87d8d36c525b01a91ce3e5e86897200ccedd
what <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-05-27 12:19:32 -0400

    corrected subscribe method argument mistake, added interface implementation check, and added New function wrapper

--- pkg/bot.go
+++ pkg/bot.go
@@ -1,13 +1,18 @@
 package pkg
 
 import (
+	"github.com/nolifejavadeveloper/disgo/internal"
 	"github.com/nolifejavadeveloper/disgo/internal/event"
+	"github.com/rs/zerolog"
 )
 
 type DiscordBot interface {
 	Start(string) error
-	Subscribe(event.Event)
+	Subscribe(event.EventHandler)
 }
 
+func NewDiscordBot(logger *zerolog.Logger) DiscordBot {
+	return internal.NewBot(logger)
+}
 
-
+var _ DiscordBot = (*internal.Bot)(nil)
