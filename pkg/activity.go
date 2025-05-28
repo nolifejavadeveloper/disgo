1282679db05771469c6e3e44a60f1e1706f74cdf
what <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-05-28 10:57:23 -0400

    added wrapper for activity model

--- pkg/activity.go
+++ pkg/activity.go
@@ -10,17 +10,59 @@ const (
 )
 
 type Activity struct {
-	Name  string `json:"name"`
-	Type  int    `json:"type"`
-	Url   string `json:"url"`
-	State string `json:"state"`
+	name  string
+	typ   int
+	url   string
+	state string
 }
 
 func NewActivity(name string, typ int, url string, state string) *Activity {
 	return &Activity{
-		Name: name,
-		Type: typ,
-		Url: url,
-		State: state,
+		name:  name,
+		typ:   typ,
+		url:   url,
+		state: state,
+	}
+}
+
+func ActivityPlaying(name string) *Activity {
+	return &Activity{
+		name: name,
+		typ:  ActivityTypePlaying,
+	}
+}
+
+func ActivityStreaming(name string) *Activity {
+	return &Activity{
+		name: name,
+		typ:  ActivityTypeStreaming,
+	}
+}
+
+func ActivityListening(name string) *Activity {
+	return &Activity{
+		name: name,
+		typ:  ActivityTypeListening,
+	}
+}
+
+func ActivityWatching(name string) *Activity {
+	return &Activity{
+		name: name,
+		typ:  ActivityTypeWatching,
+	}
+}
+
+func ActivityCustom(name string) *Activity {
+	return &Activity{
+		name: name,
+		typ:  ActivityTypeCustom,
+	}
+}
+
+func ActivityCompeting(name string) *Activity {
+	return &Activity{
+		name: name,
+		typ:  ActivityTypeCompeting,
 	}
 }
