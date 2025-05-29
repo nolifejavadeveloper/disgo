0392599b6aa735d99d3ccac6c49933d69e3993bd
what <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-05-29 08:49:17 -0400

    format and added toModel to convert to the gateway activity model

--- pkg/activity.go
+++ pkg/activity.go
@@ -1,5 +1,7 @@
 package pkg
 
+import "github.com/nolifejavadeveloper/disgo/internal/model"
+
 const (
 	ActivityTypePlaying = iota
 	ActivityTypeStreaming
@@ -10,16 +12,16 @@ const (
 )
 
 type Activity struct {
-	name  string
-	typ   int
-	url   string
+	name string
+	typ  int
+	url  string
 }
 
 func NewActivity(name string, typ int, url string, state string) *Activity {
 	return &Activity{
-		name:  name,
-		typ:   typ,
-		url:   url,
+		name: name,
+		typ:  typ,
+		url:  url,
 	}
 }
 
@@ -34,7 +36,7 @@ func ActivityStreaming(name string, url string) *Activity {
 	return &Activity{
 		name: name,
 		typ:  ActivityTypeStreaming,
-		url: url,
+		url:  url,
 	}
 }
 
@@ -63,5 +65,13 @@ func ActivityCompeting(name string) *Activity {
 	return &Activity{
 		name: name,
 		typ:  ActivityTypeCompeting,
+	}
+}
+
+func (a *Activity) toModel() *model.Activity {
+	return &model.Activity{
+		Name:  a.name,
+		Type: a.typ,
+		Url: a.url,
 	}
 }
